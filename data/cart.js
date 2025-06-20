1fe916d00dc57a8372fd121d8a2f4ea84448e3bb
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-06-20 16:38:54 +0300

    Refactor code structure for improved readability and maintainability

--- data/cart.js
+++ data/cart.js
@@ -24,10 +24,4 @@ export const devices =[
     description:'شاحن حائط سريع 45 واط المملكة المتحدة....',
     price:'49.49 ر.س'
   },
-  {
-    img:'./images/plan_camera.png',
-    title:'محولات الطاقة لفون',
-    description:'شاحن حائط سريع 45 واط المملكة المتحدة....',
-    price:'49.39 ر.س'
-  },
 ]
